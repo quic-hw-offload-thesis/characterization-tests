bccb1d966c96dbf17e230e90365f453d73fc421b
Lowie <lowie.deferme@student.uhasselt.be>
2022-07-04 13:17:06 +0000

    Add basic folder structure

---
+++ picoquic_1rtt_dec/picoquic_1rtt_dec.c
@@ -0,0 +1,9 @@
+// ------------------------------------------------------------------------------
+// Master's thesis: Hardware/software co-design for the new QUIC network protocol
+// ------------------------------------------------------------------------------
+// Characterization tests
+//
+// File: picoquic_1rtt_dec.c (c)
+// By: Lowie Deferme (UHasselt/KULeuven - FIIW)
+// On: 04 July 2022
+// ------------------------------------------------------------------------------
