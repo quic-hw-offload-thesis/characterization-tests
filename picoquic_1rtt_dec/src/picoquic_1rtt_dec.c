5c5e6178ea5799563616ec8dae59c0ba832df48f
Lowie <lowie.deferme@student.uhasselt.be>
2022-07-06 12:59:44 +0000

    Successfully link picoquic to binary

--- picoquic_1rtt_dec/src/picoquic_1rtt_dec.c
+++ picoquic_1rtt_dec/src/picoquic_1rtt_dec.c
@@ -11,10 +11,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+#include "../../libs/picoquic/picoquic/picoquic.h"
+#include "../../libs/picoquic/picoquic/picoquic_utils.h"
+#include "../../libs/picoquic/picoquic/picosocks.h"
+
+/**
+ * @brief Print current time.
+ * @details For testing out picoquic binding
+ * @return uint64_t Current time
+ */
+uint64_t print_curr_time()
+{
+    uint64_t current_time = 0;
+    current_time = picoquic_current_time();
+    printf("Current time: %llu\n", current_time);
+    return current_time;
+}
 
 int main(int argc, char **argv)
 {
     int exit_code = 0;
+    print_curr_time();
     printf("picoquic_1rtt_dec has ran\n");
     exit(exit_code);
 }
