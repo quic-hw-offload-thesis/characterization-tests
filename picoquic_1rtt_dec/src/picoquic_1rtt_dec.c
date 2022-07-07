30d18a2c175df96f2db2e75be7c0553435564556
Lowie <lowie.deferme@student.uhasselt.be>
2022-07-07 16:03:58 +0000

    wip (segfaulting)

--- picoquic_1rtt_dec/src/picoquic_1rtt_dec.c
+++ picoquic_1rtt_dec/src/picoquic_1rtt_dec.c
@@ -38,24 +38,58 @@ picoquic_quic_t *create_picoquic_context()
  * @param dcid Dcid of new connection
  * @return picoquic_cnx_t* new connection
  */
-picoquic_cnx_t *add_connection_to_context(picoquic_quic_t *context, picoquic_connection_id_t *dcid)
+picoquic_cnx_t *add_connection_to_context(picoquic_quic_t *context, picoquic_connection_id_t *dcid) // Todo: add keys to connection creation function
 {
     // Todo: create connection and add to context
     return NULL;
 }
 
+// Pointers to in en out buffer
+int buffer_size = 2048;
+uint8_t *bytes_in;
+uint8_t *bytes_out;
+
 int main(int argc, char **argv)
 {
+    // Set initial exit code to success
     int exit_code = 0;
 
-    picoquic_quic_t *quic_context = create_picoquic_context();
-    uint8_t *bytes;
+    // Allocate memory for in/out buffers
+    bytes_in = malloc(buffer_size);
+    bytes_out = malloc(buffer_size);
+    memset(bytes_in, 0, buffer_size);
+    memset(bytes_out, 0, buffer_size);
 
-    // picoquic_parse_header_and_decrypt(
-    //     quic_context,
+    // Todo: Read data input file to buffer
 
-    // );
+    // Create picoquic context
+    picoquic_quic_t *quic = create_picoquic_context();
+
+    // Todo: Add connections to context
+
+    // Create parameters for `picoquic_parse_header_and_decrypt`
+    uint64_t current_time = picoquic_current_time();
+    picoquic_stream_data_node_t *decrypted_data = picoquic_stream_data_node_alloc(quic);
+
+    // Parse & decrypt bytes_in
+    picoquic_parse_header_and_decrypt(
+        quic,                    // Picoquic context (quic)
+        bytes_in,                // (bytes)
+        0,                       // (length)
+        0,                       // (packet_length)
+        NULL,                    // (addr_from)
+        picoquic_current_time(), // Current time (current_time)
+        decrypted_data,          // (decrypted_data)
+        NULL,                    // Packet header (ph)
+        NULL,                    // (pcnx)
+        NULL,                    // (consumed)
+        NULL                     // (new_context_created)
+    );
 
     printf("picoquic_1rtt_dec has ran\n");
+
+    // Cleanup
+    free(bytes_in);
+    free(bytes_out);
     exit(exit_code);
 }
