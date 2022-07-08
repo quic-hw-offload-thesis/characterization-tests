38c066e4b152980fa35e7854d5e100431dbdfd10
Lowie <lowie.deferme@student.uhasselt.be>
2022-07-08 10:27:55 +0000

    wip

--- picoquic_1rtt_dec/src/picoquic_1rtt_dec.c
+++ picoquic_1rtt_dec/src/picoquic_1rtt_dec.c
@@ -32,7 +32,7 @@
  */
 uint8_t msg[] = "\x6d\xf3\xde\xc1\xbb\x98\xd9\x2f\x74\x02\xe8\x98\xce\xc8\x79\x54\x2c\xbd\x28\x23\x4c\x76\xb8\x64\x5e\xe1\xee\xe1\xc3\x7d\xb9\xc7\x79\x11\x2a\xe8\xbb\x5a\xfa\xaa\x6c\xbb\x71\xd3\xf0\xcd\x29\xbe\x3a\xb0\x40\x14\x65\x86\x30\xe8\xc0\x2b\xfc\x75\xb5\xe3\x22\xc2\xa4\x59\x32\x17\x02\xc2\x98\x52\x07\x94\x3d\x2b\x4b\xef\xbf\x29\x78\x7f\x2d\xdd\x2b\x74\x9d";
 
-// Pointers to in en out buffer
+// global pointers to input and output buffer
 int buffer_size = 1024;
 uint8_t *bytes_in;
 uint8_t *bytes_out;
@@ -42,6 +42,10 @@ int main(int argc, char **argv)
     // Set initial exit code to success
     int exit_code = 0;
 
+    // Set object's pointer to nullpointer initially
+    picoquic_quic_t *quic = NULL;
+    picoquic_stream_data_node_t *decrypted_data = NULL;
+
     // Allocate memory for input buffer
     bytes_in = malloc(buffer_size);
     memset(bytes_in, 0, buffer_size);
@@ -50,69 +54,81 @@ int main(int argc, char **argv)
     memcpy(bytes_in, &msg, sizeof(msg)); // Fixme: Set fixed msg to input buffer for debug, should be read from file for final test
 
     // Create picoquic context
-    // picoquic_quic_t *quic = create_picoquic_context();
-    picoquic_quic_t *quic = NULL;
-
-    // Todo: Add connections to context
-
-    int length = sizeof(msg);   // Fixme: Set length to msg len for debug, should be set to len of nth packet for final test
-    int packet_length = length; // Parse one packet at a time
-
-    // Create parameters for `picoquic_parse_header_and_decrypt`
-    // Source: https://github.com/private-octopus/picoquic/blob/28b313c1ee483bfae784d33593d1e56a32701cc4/picoquic/packet.c#L2135
-    picoquic_cnx_t *cnx = NULL;
-    picoquic_packet_header ph;
-    int new_context_created = 0;
-    picoquic_stream_data_node_t *decrypted_data = picoquic_stream_data_node_alloc(quic); // BUG: Segfault happens here since `quic` is `NULL`
-    size_t consumed = 0;                                                                 // Source: https://github.com/private-octopus/picoquic/blob/28b313c1ee483bfae784d33593d1e56a32701cc4/picoquic/packet.c#L2505
-    uint64_t current_time = picoquic_current_time();                                     // Current time is calculated as last before function call
-
-    //  Parse & decrypt bytes_in into bytes_out
-    picoquic_parse_header_and_decrypt(
-        quic,                // quic: Picoquic context
-        bytes_in,            // bytes: Input bytes
-        length,              // length: Length of input bytes
-        packet_length,       // packet_length: Total packet length
-        NULL,                // addr_from:
-        current_time,        // current_time: Current time
-        decrypted_data,      // decrypted_data: picoquic_stream_data_node_t to fill
-        &ph,                 // ph: picoquic_packet_header to fill
-        &cnx,                // pcnx:
-        &consumed,           // consumed: Amount of consumed bytes?
-        &new_context_created // new_context_created: 0 if there isn't a new context created?
+    uint64_t current_time = picoquic_current_time();
+    quic = picoquic_create( // Source: https://github.com/private-octopus/picoquic/blob/28b313c1ee483bfae784d33593d1e56a32701cc4/picoquictest/parseheadertest.c#L568
+        8,                  // uint32_t max_nb_connections
+        NULL,               // char const* cert_file_name,
+        NULL,               // char const* key_file_name,
+        NULL,               // char const * cert_root_file_name
+        NULL,               // char const* default_alpn
+        NULL,               // picoquic_stream_data_cb_fn default_callback_fn
+        NULL,               // void* default_callback_ctx
+        NULL,               // picoquic_connection_id_cb_fn cnx_id_callback
+        NULL,               // void* cnx_id_callback_data
+        NULL,               // uint8_t reset_seed[PICOQUIC_RESET_SECRET_SIZE]
+        current_time,       // uint64_t current_time
+        &current_time,      // uint64_t* p_simulated_time
+        NULL,               // char const* ticket_file_name
+        NULL,               // const uint8_t* ticket_encryption_key
+        0                   // size_t ticket_encryption_key_length
     );
-    bytes_out = decrypted_data->data; // Fixme: Set pointer for debug, should append for final test
 
-    printf("picoquic_1rtt_dec has ran\n");
+    if (quic != NULL)
+    {
+        // Todo: Add connections to context
+
+        // Create parameters for `picoquic_parse_header_and_decrypt`
+        picoquic_stream_data_node_t *decrypted_data = picoquic_stream_data_node_alloc(quic);
+        if (decrypted_data != NULL)
+        {
+            int length = sizeof(msg);   // Fixme: Set length to msg len for debug, should be set to len of nth packet for final test
+            int packet_length = length; // Parse one packet at a time
+            // Source: https://github.com/private-octopus/picoquic/blob/28b313c1ee483bfae784d33593d1e56a32701cc4/picoquic/packet.c#L2135
+            picoquic_cnx_t *cnx = NULL;
+            picoquic_packet_header ph;
+            int new_context_created = 0;
+            size_t consumed = 0;                             // Source: https://github.com/private-octopus/picoquic/blob/28b313c1ee483bfae784d33593d1e56a32701cc4/picoquic/packet.c#L2505
+            uint64_t current_time = picoquic_current_time(); // Current time is calculated as last before function call
+
+            //  Parse & decrypt bytes_in into bytes_out
+            printf("[INFO] Calling picoquic_parse_header_and_decrypt\n");
+            picoquic_parse_header_and_decrypt(
+                quic,                // quic: Picoquic context
+                bytes_in,            // bytes: Input bytes
+                length,              // length: Length of input bytes
+                packet_length,       // packet_length: Total packet length
+                NULL,                // addr_from:
+                current_time,        // current_time: Current time
+                decrypted_data,      // decrypted_data: picoquic_stream_data_node_t to fill
+                &ph,                 // ph: picoquic_packet_header to fill
+                &cnx,                // pcnx:
+                &consumed,           // consumed: Amount of consumed bytes?
+                &new_context_created // new_context_created: 0 if there isn't a new context created?
+            );
+            bytes_out = decrypted_data->data; // Fixme: Set pointer for debug, should append for final test
+        }
+        else
+        {
+            printf("[ERROR] picoquic_stream_data_node_alloc for decrypted_data failed\n");
+        }
+    }
+    else
+    {
+        printf("[ERROR] picoquic_create failed\n");
+    }
 
     // Cleanup
     free(bytes_in);
-    picoquic_stream_data_node_recycle(decrypted_data); // Source: https://github.com/private-octopus/picoquic/blob/28b313c1ee483bfae784d33593d1e56a32701cc4/picoquic/packet.c#L2400
+    if (decrypted_data != NULL)
+    {
+        picoquic_stream_data_node_recycle(decrypted_data);
+    }
+    if (quic != NULL)
+    {
+        picoquic_free(quic);
+    }
+
+    // Exit program
+    printf("[INFO] Function \"%s\" in \"%s\" is finished\n", __FUNCTION__, __FILE__);
     exit(exit_code);
 }
-
-// /**
-//  * @brief Create a picoquic context object
-//  *
-//  * @return picoquic_quic_t* picoquic context
-//  */
-// picoquic_quic_t *create_picoquic_context()
-// {
-//     picoquic_quic_t *quic_context = NULL;
-//     uint64_t current_time = picoquic_current_time();
-//     // Todo: create and configure quic context
-//     return NULL;
-// }
-
-// /**
-//  * @brief Add new connection to context
-//  *
-//  * @param context Context which to add the new connection to
-//  * @param dcid Dcid of new connection
-//  * @return picoquic_cnx_t* new connection
-//  */
-// picoquic_cnx_t *add_connection_to_context(picoquic_quic_t *context, picoquic_connection_id_t *dcid) // Todo: add keys to connection creation function
-// {
-//     // Todo: create connection and add to context
-//     return NULL;
-// }
